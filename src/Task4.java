782b80293b9d497ceaad2e2432077a5fc20e924e
Maks <petrenkomaks405@gmail.com>
2019-06-17 16:36:07 +0300

    Test 4

---
+++ src/Task4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Task4 {
+public static void main(String []args){
+Scanner s = new Scanner(System.in);
+String a = s.nextLine();
+String b = s.nextLine();
+String c = s.nextLine();
+String d = s.nextLine();
+String e = s.nextLine();
+String f = s.nextLine();
+String g = s.nextLine();
+String h = s.nextLine();
+String i = s.nextLine();
+String j = s.nextLine();
+String k = s.nextLine();
+String l = s.nextLine();
+String m = s.nextLine();
+String n = s.nextLine();
+
+System.out.println("\n"+a+b+c+d+e+f+g+h+i+j+k+l+"\t"+m+n);
+    }
+}
