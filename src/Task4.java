3c3a52e22406f4ab0edd24507d134b51cd5c418e
Maks <petrenkomaks405@gmail.com>
2019-08-07 22:43:36 +0300

    Correct mistaces

--- src/Task4.java
+++ src/Task4.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 public class Task4 {
 public static void main(String []args){
 Scanner s = new Scanner(System.in);
-String a = s.nextLine();
-String b = s.nextLine();
-String c = s.nextLine();
-String d = s.nextLine();
-String e = s.nextLine();
-String f = s.nextLine();
-String g = s.nextLine();
-String h = s.nextLine();
-String i = s.nextLine();
-String j = s.nextLine();
-String k = s.nextLine();
-String l = s.nextLine();
-String m = s.nextLine();
-String n = s.nextLine();
+char a = s.next().charAt(0);
+char b = s.next().charAt(0);
+char c = s.next().charAt(0);
+char d = s.next().charAt(0);
+char e = s.next().charAt(0);
+char f = s.next().charAt(0);
+char g = s.next().charAt(0);
+char h = s.next().charAt(0);
+char i = s.next().charAt(0);
+char j = s.next().charAt(0);
+char k = s.next().charAt(0);
+char l = s.next().charAt(0);
+char m = s.next().charAt(0);
+char n = s.next().charAt(0);
 
 System.out.println("\n"+a+b+c+d+e+f+g+h+i+j+k+l+"\t"+m+n);
     }
